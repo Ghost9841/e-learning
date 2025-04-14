730103439cb0375b785021272926d6ef441d1380
Ghost9841 <asubedi778@gmail.com>
2025-04-14 13:59:04 +0545

    Added Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
  export default {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily:{
+          poppins: ["Poppins","sans-seif"],
+        },
+        colors:{
+          primary: "#f7ba34",
+          secondary:"#69a79c",
+          light:"#f7f7f7",
+          dark:"333333",
+          dark2:"999999",
+        }
+      },
     },
     plugins: [],
   }
