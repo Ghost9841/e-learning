8e7c8e6c6996a898a463271df91bdd07f93f8457
Ghost9841 <asubedi778@gmail.com>
2025-04-14 14:07:46 +0545

    Adding Shadcn (as the tailwind is v4 dunno if this works or not

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@
           light:"#f7f7f7",
           dark:"333333",
           dark2:"999999",
+        },
+        containers:{
+          center: true,
+          DEFAULT:"1rem",
+          sm:"2rem",
+          lg:"4rem",
+          xl:"5rem",
+          "2xl":"6rem",
         }
       },
     },
